54d5b83c6675fad925bfb99f379440318502fd09
Gustavo Alviarez <gusalv2001@gmail.com>
2022-04-13 17:58:54 -0400

    first whole page

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,25 @@
 import Layout from "../components/layouts/main";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
+import { AnimatePresence } from "framer-motion";
+import Fonts from "../components/fonts";
 
 function Website({ Component, pageProps, router }) {
   return (
     <ChakraProvider theme={theme}>
       <Layout>
-        <Component {...pageProps} key={router.route} />
+        <Fonts />
+        <AnimatePresence
+          exitBeforeEnter
+          initial={true}
+          onExitComplete={() => {
+            if (typeof window !== "undefined") {
+              window.scrollTo({ top: 0 });
+            }
+          }}
+        >
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
       </Layout>
     </ChakraProvider>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
-import { Box, Container, Heading } from "@chakra-ui/react";
+import {
+  Link,
+  Box,
+  Button,
+  Container,
+  Heading,
+  Image,
+  SimpleGrid,
+  List,
+  ListItem,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import Section from "../components/section";
+import { GridItem } from "../components/grid-item";
+import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from "react-icons/io5";
 
 function Home() {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
+      >
         hello, Im gustavo
       </Box>
       <Box display={{ md: "flex" }}>
@@ -14,6 +31,95 @@ function Home() {
           <p>Frontend Developer</p>
         </Box>
       </Box>
+
+      <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} align="center">
+        <Image
+          borderColor="whiteAlpha.800"
+          borderWidth={2}
+          borderStyle="solid"
+          display="inline-block"
+          maxWidth="100px"
+          borderRadius="full"
+          src="../public/gustavo.jpg"
+          alt="Profile image"
+        ></Image>
+      </Box>
+
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <p>paragraph</p>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I ♥
+        </Heading>
+        <p>
+          Art, Music,{" "}
+          <Link href="https://illust.odoruinu.net/" target="_blank">
+            Drawing
+          </Link>
+          , Playing Drums,{" "}
+          <Link href="https://500px.com/p/craftzdog" target="_blank">
+            Photography
+          </Link>
+          , Leica, Machine Learning
+        </p>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          On the web
+        </Heading>
+        <List>
+          <ListItem>
+            <Link href="https://github.com/craftzdog" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoGithub />}
+              >
+                @craftzdog
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://twitter.com/inkdrop_app" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoTwitter />}
+              >
+                @inkdrop_app (English)
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://twitter.com/craftzdog" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoTwitter />}
+              >
+                @craftzdog (日本語)
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://instagram.com/craftzdog" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoInstagram />}
+              >
+                @craftzdog
+              </Button>
+            </Link>
+          </ListItem>
+        </List>
+      </Section>
     </Container>
   );
 }
